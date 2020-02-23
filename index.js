61b19ed4e063c1f885b237f0da6e4b9a95f7c678
acaciomartins <acacio.am@gmail.com>
2020-02-23 19:14:10 -0300

    Ajust layout

--- index.js
+++ index.js
@@ -6,26 +6,26 @@ export default function SimpleTable(props) {
 
     useEffect(() => {
         setColumns(props.headerData);
-    }, [props.itemVenda])
+    }, [props.data])
 
     return (
         <>
             {
-                props.itemVenda && props.itemVenda.length > 0 ? (
-                    <View style={styles.tabela} >
+                props.data && props.data.length > 0 ? (
+                    <View style={styles.table} >
                         {props.headerData && props.headerData.map(hd => (
-                            <Text style={[styles.tabelaTexto, { width: hd.size, backgroundColor: '#c0c0c0' }]}>{hd.name}</Text>
+                            <Text style={[styles.tableText, { width: hd.size, backgroundColor: '#c0c0c0' }]}>{hd.name}</Text>
 
                         ))}
                     </View>
                 ) :
-                    <Text style={[{ fontSize: 10, textAlign: 'center', }]}>Não há itens adicinoados</Text>
+                    <Text style={[{ fontSize: 10, textAlign: 'center', }]}>List Empty</Text>
             }
             {
-                props.itemVenda && props.itemVenda.map(item => (
-                    <View style={styles.tabela}>
+                props.data && props.data.map(item => (
+                    <View style={styles.table}>
                         {props.columnData && props.columnData.map((hd, index) => (
-                            <Text style={[styles.tabelaTexto, { width: columns[index].size }]}>{item[hd]}{}</Text>
+                            <Text style={[styles.tableText, { width: columns && columns[index] && columns[index].size }]}>{item[hd]}{}</Text>
 
                         ))}
                     </View>
@@ -37,17 +37,14 @@ export default function SimpleTable(props) {
 
 
 const styles = StyleSheet.create({
-
-
-    tabela: {
+    table: {
         flex: 1,
         flexWrap: 'nowrap',
         flexDirection: 'row',
 
 
     },
-
-    tabelaTexto: {
+    tableText: {
         padding: 0,
         fontSize: 10,
     },
